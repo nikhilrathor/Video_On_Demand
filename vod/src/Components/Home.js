cc0d99420b3aaeccbde9bb174118cb0c58057138
nikhilrathor <nikhilrathor01@gmail.com>
2021-06-27 10:05:40 +0530

    data fetched from API using axios

--- vod/src/Components/Home.js
+++ vod/src/Components/Home.js
@@ -1,5 +1,21 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 const Home = () =>{
+
+    const [videos, setVideos] = useState(null);
+
+    useEffect(()=>{
+        axios.post("https://videoapi-dot-virtualeventdemo.el.r.appspot.com/")
+        .then(res=>{
+            let arr = res.data.result;
+            setVideos(arr);
+        })
+        .catch(err=>{
+            console.log("Something went wrong");
+        })
+    })
+
     return(
         <div>
             Home
